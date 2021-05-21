43c18ee8b79d126b9c01f4035d95efe5815bcb33
殿小二 <dianxiaoer@mandofin.com>
2021-05-21 17:24:36 +0800

    jackson序列化验证

--- src/main/java/com/eghm/cloud/stream/Config.java
+++ src/main/java/com/eghm/cloud/stream/Config.java
@@ -1,5 +1,11 @@
 package com.eghm.cloud.stream;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.json.JsonWriteFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
@@ -47,5 +53,22 @@ public class Config {
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer bigDecimalJackson2ObjectMapperBuilderCustomizer () {
         return jacksonObjectMapperBuilder -> jacksonObjectMapperBuilder.serializerByType(BigDecimal.class, ToStringSerializer.instance);
+    }
+    
+    /**
+     * 对象是BigDecimal时如果转为字符串形式的json,
+     * 第一种方式: 在配置文件中generator添加配置
+     * 第二种方式: 创建Jackson2ObjectMapperBuilderCustomizer的bean进行定制化类型的特殊处理(推荐)
+     * 第三种方式: 直接设置ObjectMapper的配置属性(不推荐)
+     */
+    public static void main(String[] args) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        // mapper.configure(JsonWriteFeature.WRITE_NUMBERS_AS_STRINGS.mappedFeature(), true);
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(BigDecimal.class, ToStringSerializer.instance);
+        mapper.registerModule(module);
+        UserBean bean = new UserBean();
+        bean.setAge(12);
+        System.out.println(mapper.writeValueAsString(bean));
     }
 }
